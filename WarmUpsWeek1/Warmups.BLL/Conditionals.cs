39cdfe3487ace04a798c8a7dda79228bd684fbe6
RyanSheehan22 <RyanSheehan22@gmail.com>
2018-01-18 23:15:09 -0600

    warmup work to Q21

--- WarmUpsWeek1/Warmups.BLL/Conditionals.cs
+++ WarmUpsWeek1/Warmups.BLL/Conditionals.cs
@@ -25,8 +25,6 @@ namespace Warmups.BLL
 
         public int Diff21(int n)
         {
-            int Diff21;
-
             if (21 < n)
             {
                 return Math.Abs(2 * (n - 21));
@@ -73,81 +71,173 @@ namespace Warmups.BLL
         {
             bool isPosNeg = true;
 
-            //if (a > 0 || b > 0)
-            //{
-            //    return isPosNeg;
-            //}
-            //else if (a > 0 && b > 0)
-            //{
-            //    return negative = true;
-            //}
-            //return false;
-
+            if (a > 0 || b > 0)
+            {
+                return isPosNeg;
+            }
+            else if (negative = true && (a > 0 && b > 0));
+            {
+                return isPosNeg;
+            }
         }
 
         public string NotString(string s)
         {
-            throw new NotImplementedException();
+
+            if (s.Contains("not"))
+            {
+                return s;
+            } else
+            {
+                return ("not " + s);
+            }
         }
 
         public string MissingChar(string str, int n)
         {
-            throw new NotImplementedException();
+            return str.Remove(n, 1);
         }
 
         public string FrontBack(string str)
         {
             throw new NotImplementedException();
         }
+        //{
+        //    char front;
+        //    char back;
+
+        //    string flipped = str.ToCharArray();
+
+        //}
 
         public string Front3(string str)
         {
-            throw new NotImplementedException();
-        }
+            string Front3 = (str + str + str);
+
+            if (str.Length < 3)
+            {
+                return Front3;
+            }
+            else
+            {
+                return str.Substring(0, 3) + str.Substring(0, 3) + str.Substring(0, 3);
+            }
+        } 
 
         public string BackAround(string str)
         {
-            throw new NotImplementedException();
+            char last = str[str.Length - 1];
+
+            string backAround = (last + str + last);
+
+            return backAround;
         }
 
         public bool Multiple3or5(int number)
         {
-            throw new NotImplementedException();
+            bool multiple3or5 = true;
+
+            bool mult3 = (number % 3 == 0);
+            bool mult5 = (number % 5 == 0);
+
+            if (mult3 || mult5)
+            {
+                return multiple3or5;
+            }
+
+            return false;
         }
 
         public bool StartHi(string str)
         {
+            //bool startHi = true;
+
+            //if (str.StartsWith("hi"))
+            //{
+            //    return startHi;
+            //}
+            //return false;
+
             throw new NotImplementedException();
+
         }
 
         public bool IcyHot(int temp1, int temp2)
         {
-            throw new NotImplementedException();
+            bool icyHot = true;
+
+            if (temp1 < 0 && temp2 > 100)
+            {
+                return icyHot;
+            }
+            else if (temp1 > 100 && temp2 < 0)
+            {
+                return icyHot;
+            }
+            return false;
         }
 
         public bool Between10and20(int a, int b)
         {
-            throw new NotImplementedException();
+            bool between = true;
+
+            if ((a > 10 && a < 20) || (b > 10 && b < 20))
+            {
+                return between;
+            }
+            return false;
         }
 
         public bool HasTeen(int a, int b, int c)
         {
-            throw new NotImplementedException();
+            bool teen = true;
+
+            if ((a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19))
+            {
+                return teen;
+            }
+            return false;
         }
 
         public bool SoAlone(int a, int b)
         {
-            throw new NotImplementedException();
+            bool soAlone = true;
+            bool notAlone = false;
+
+            if ((a >= 13 && a <= 19) && (b >= 13 && b <= 19))
+            {
+                return notAlone;
+            }
+            else if ((a >= 13 && a <= 19) || (b >= 13 && b <= 19))
+            {
+                return soAlone;
+            }
+            return false;
         }
 
         public string RemoveDel(string str)
         {
-            throw new NotImplementedException();
+            if (str.Contains("del"))
+            {
+                string removeDel = str.Remove(1, 3);
+                return removeDel;
+            }
+            return str;
         }
 
         public bool IxStart(string str)
         {
+            //bool isLetter = true;
+            //string letters = 
+
+            //if (str.Contains("\w"+"ix"))
+            //{
+            //    return isLetter;
+            //}
+            //return false;
+
             throw new NotImplementedException();
+
         }
 
         public string StartOz(string str)
