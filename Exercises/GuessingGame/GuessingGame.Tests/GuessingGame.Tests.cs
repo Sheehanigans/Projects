6a09ebf428e43d83a0b39ae0f98f936e2b87ebeb
RyanSheehan22 <RyanSheehan22@gmail.com>
2018-01-24 22:36:07 -0600

    better guessing game

--- Exercises/GuessingGame/GuessingGame.Tests/GuessingGame.Tests.cs
+++ Exercises/GuessingGame/GuessingGame.Tests/GuessingGame.Tests.cs
@@ -20,5 +20,35 @@ namespace GuessingGame.Tests
             GuessResult actual = gameInstance.ProcessGuess(152);
             Assert.AreEqual(GuessResult.Invalid, actual);
         }
+
+        [Test]
+        public void HigherGuessResultTest()
+        {
+            GameManager gameInstance = new GameManager();
+            gameInstance.Start(10);
+
+            GuessResult actual = gameInstance.ProcessGuess(8);
+            Assert.AreEqual(GuessResult.Higher, actual);
+        }
+
+        [Test]
+        public void LowerGuessResultTest()
+        {
+            GameManager gameInstance = new GameManager();
+            gameInstance.Start(10);
+
+            GuessResult actual = gameInstance.ProcessGuess(12);
+            Assert.AreEqual(GuessResult.Lower, actual);
+        }
+
+        [Test]
+        public void VictoryGuessResultTest()
+        {
+            GameManager gameInstance = new GameManager();
+            gameInstance.Start(10);
+
+            GuessResult actual = gameInstance.ProcessGuess(10);
+            Assert.AreEqual(GuessResult.Victory, actual);
+        }
     }
 }
--- Exercises/GuessingGame/GuessingGame/Program.cs
+++ Exercises/GuessingGame/GuessingGame/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GuessingGame.DLL;
 
 namespace GuessingGame
 {
@@ -10,52 +11,8 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            int theAnswer;
-            int playerGuess;
-            string playerInput;
-            string playerName;
-
-            Random r = new Random();
-            theAnswer = r.Next(1, 21);
-
-            Console.WriteLine("What is your name?");
-            //Console.ReadLine(playerName);
-
-            do
-            {
-                // get player input
-                Console.Write("Enter your guess (1-20): ");
-                playerInput = Console.ReadLine();
-
-                //attempt to convert the string to a number
-                if (int.TryParse(playerInput, out playerGuess))
-                {
-                    if (playerGuess == theAnswer)
-                    {
-                        Console.WriteLine($"{theAnswer} was the number.  You Win!");
-                        break;
-                    }
-                    else
-                    {
-                        if (playerGuess > theAnswer)
-                        {
-                            Console.WriteLine("Your guess was too High!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Your guess was too low!");
-                        }
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("That wasn't a number!");
-                }
-
-            } while (true);
-
-            Console.WriteLine("Press any key to quit.");
-            Console.ReadKey();
+            GameFlow game = new GameFlow();
+            game.PlayGame();
         }
     }
 }
