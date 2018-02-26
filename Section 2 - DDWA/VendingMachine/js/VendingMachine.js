1f1b83cd3235dade3601f3d2b30b32dd629f2232
RyanSheehan22 <RyanSheehan22@gmail.com>
2018-02-25 21:05:03 -0600

    finished vending machine

--- Section 2 - DDWA/VendingMachine/js/VendingMachine.js
+++ Section 2 - DDWA/VendingMachine/js/VendingMachine.js
@@ -1,7 +1,19 @@
 $(document).ready(function (){
 
     loadGoodies();
+    displayMoney(0)
+    $('#returnChange').hide()
 })
+
+//global variables 
+
+var moneyInSystem = 0;
+var itemIdNumber = 0;
+
+var quartersInSystem = 0;
+var dimesInSystem = 0;
+var nickelsInSystem = 0;
+var penniesInSystem = 0;
 
 function loadGoodies(){
 
@@ -19,20 +31,20 @@ function loadGoodies(){
                 var price = item.price;
                 var quantity = item.quantity;
 
-                var goodie = '<button class="col-md-4 btn btn-default" onclick="getGoodie(' + id + ')">';
-                    goodie += '<p>' + id + '</p>' + '<br/>';
-                    goodie += '<p>' + name + '</p>' + '<br/>';
-                    goodie += '<p>' + price + '</p>' + '<br/>';
-                    goodie += '<p>' + quantity + '</p>' + '<br/>';
+                var goodie = '<button class="col-md-3 btn btn-default goodieButtons" id="goodieButtonId'+ id +'" onclick="getGoodie(' + id + ')">';
+                    goodie += '<div class="textBox">'
+                    goodie += '<p id="itemId">' + id + '</p>' + '<br/>';
+                    goodie += '<h3 class="itemName">' + name + '</h3>' + '<br/>';
+                    goodie += '<p>' + '$' + parseFloat(price).toFixed(2) + '</p>' + '<br/>';
+                    goodie += '<p>' + 'Quantity left: ' + quantity + '</p>' + '<br/>';
+                    goodie += '</div>'
                     goodie += '</button>';
 
                 goodieButtons.append(goodie);
             });
         },
         error: function(){
-            $('#messageDisplay')
-            .append($('<p>')
-            .text('Error calling web service. Please try again later.'));
+            displayConnectionError()
         }
     });
 }
@@ -41,29 +53,36 @@ function clearGoodies(){
     $('#goodieSelection').empty();
 }
 
-var moneyInSystem = 0;
-
 function addMoney(buttonId){    
 
+    $("#displayChange").empty()
+
     if(buttonId == 1){
-        moneyInSystem += 1.00;
+        moneyInSystem += 1;
+        quartersInSystem += 4;
     }
     else if(buttonId == 2){
         moneyInSystem += 0.25;
+        quartersInSystem += 1;
     }
     else if(buttonId == 3){
         moneyInSystem += 0.10;
+        dimesInSystem += 1;
     }
     else if (buttonId == 4){
-        moneyInSystem += 0.05;    
+        moneyInSystem += 0.05;
+        nickelsInSystem += 1;
     }
 
     displayMoney(moneyInSystem);
+    $('#returnChange').show()
 }
 
 function getGoodie(itemId){
 
+    $("#displayChange").empty()
     displayItem(itemId)
+    itemIdNumber = itemId
 
     var amountToSend = parseFloat(moneyInSystem).toFixed(2)
 
@@ -87,20 +106,70 @@ function getGoodie(itemId){
             pennies *= 0.01
 
             var change = quarters + dimes + nickels + pennies
+
             displayChange(change)
             displayMessage("Thank you!")
             loadGoodies()
-            moneyInSystem = 0
-            displayMoney(0)
+            resetForm()
         },
-        error: function(data, status){
-            var message = status.message
-            alert(message)
-            
-            //displayMessage(message)
+        error: function(jqXHR, status, err){
+            if(err == "Unprocessable Entity"){
+                var message = jqXHR.responseText.substr(12).replace('"','').replace('}','')
+                displayMessage(message)
+            }
+            else{
+                displayConnectionError()
+            }
         }
     })
 }
+
+$("#makePurchase").on("click", function(){
+    if (itemIdNumber > 0){
+        getGoodie(itemIdNumber)
+    }
+})
+
+$("#returnChange").on("click", function(){
+
+    quartersInSystem *= 0.25
+    dimesInSystem *= 0.10
+    nickelsInSystem *= 0.05
+    penniesInSystem *= 0.01
+
+    var change = quartersInSystem + dimesInSystem + nickelsInSystem + penniesInSystem
+
+    displayChange(change)
+    resetForm()
+    $('#messageDisplay').empty()
+
+    // var changeToReturn = moneyInSystem
+
+    // var quarters = 0
+    // var dimes = 0
+    // var nickels = 0
+    // var pennies = 0
+
+    // while(changeToReturn > 0)
+    // {
+    //     if(changeToReturn % 0.25 == 0){
+    //         changeToReturn -= 0.25
+    //         quarters++
+    //     }
+    //     else if(changeToReturn % 0.10 == 0){
+    //         changeToReturn -= 0.10
+    //         dimes++
+    //     }
+    //     else if(changeToReturn % 0.05 == 0){
+    //         changeToReturn -= 0.05
+    //         nickels++
+    //     }
+    //     else if(changeToReturn % 0.01 == 0){
+    //         changeToReturn -= 0.01
+    //         pennies++
+    //     }
+    // }
+})
 
 function displayChange(change){
     $('#displayChange')
@@ -128,4 +197,27 @@ function displayMessage(message){
     .empty()
     .append($('<h4>')
     .text(message))
+}
+
+function displayConnectionError(){
+    $('#messageDisplay')
+    .empty()
+    .append($('<p style="color: red">')
+    .text('Error calling web service. Please try again later.'));
+}
+
+function resetForm(){
+    $('#moneyDisplay').empty()
+    moneyInSystem = 0;
+    displayMoney(0)
+
+    $('#itemDisplay').empty()
+    itemIdNumber = 0
+
+    $('#returnChange').hide()
+
+    quartersInSystem = 0
+    dimesInSystem = 0
+    nickelsInSystem = 0
+    penniesInSystem = 0
 }
